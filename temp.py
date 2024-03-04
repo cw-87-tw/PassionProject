3e6a53560d57b50a334ad5d335ddbbe2a4875db1
oTaiwan <66476837+cw-87-tw@users.noreply.github.com>
2024-03-04 12:40:19 +0800

    testing

--- temp.py
+++ temp.py
@@ -1,14 +1,41 @@
-import twstock # pip install twstock, lxml
-import pandas as pd # pip install pandas
-target_stock = '0050'
-stock = twstock.Stock(target_stock)
-target_price = stock.fetch_from(2023, 5)
+from selenium import webdriver
+from os import system as sys
+from time import sleep
+driver = webdriver.Firefox()
 
-print(target_price)
+# tools
+def clickButton(xpath): driver.find_element(by = "xpath", value = xpath).click()
+def boxType(xpath, msg):
+    box = driver.find_element(by = "xpath", value = xpath)
+    box.send_keys(msg)
+def getText(xpath): return driver.find_element(by = "xpath", value = xpath).text
 
-name_attribute = ['Date', 'Capacity', 'Turnover', 'Open', 'High', 'Low', 'Close', 'Change', 'Transcation']
-# 日期 總成交股數 總成交金額(Volume) 開 高 低 收 漲跌幅 成交量
+# process
+def login():
+    driver.get('https://statementdog.com/users/sign_in')
+    boxType(r'//*[@id="user_email"]', "h1110539@stu.wghs.tp.edu.tw")
+    boxType(r'//*[@id="user_password"]', "passion")
+    clickButton("/html/body/div[3]/div[1]/form/div/button")
+    sleep(1)
 
-df = pd.DataFrame(columns = name_attribute, data = target_price)
+def setSeasonly():
+    driver.get("https://statementdog.com/analysis/2330/monthly-revenue")
+    sleep(1)
+    clickButton(r"/html/body/div[2]/div[2]/div/div[2]/div/div[2]/div[2]/div/div[1]/div[2]/div[1]/div[1]/table/tr/td[1]/div[1]/i")
+    clickButton(r"/html/body/div[2]/div[2]/div/div[2]/div/div[2]/div[2]/div/div[1]/div[2]/div[1]/div[1]/table/tr/td[1]/ul/li[4]")
+    clickButton(r"/html/body/div[2]/div[2]/div/div[2]/div/div[2]/div[2]/div/div[1]/div[2]/div[1]/div[1]/table/tr/td[1]/div[2]/div[1]/select/option[14]")
+    clickButton(r"/html/body/div[2]/div[2]/div/div[2]/div/div[2]/div[2]/div/div[1]/div[2]/div[1]/div[1]/table/tr/td[1]/div[2]/div[2]/select/option[23]")
+    clickButton(r"/html/body/div[2]/div[2]/div/div[2]/div/div[2]/div[2]/div/div[1]/div[2]/div[1]/div[1]/table/tr/td[1]/div[2]/div[3]/div")
 
-print(df)
+def test():
+    driver.get("https://statementdog.com/analysis/2330/income-statement")
+    sleep(1)
+    print(getText(r"/html/body/div[2]/div[2]/div/div[2]/div/div[2]/div[2]/div/div[1]/div[2]/div[2]/div[1]/ul/li[2]/table/tr[2]/td[1]"))
+
+
+login()
+setSeasonly()
+test()
+# sys("pause")
+# cookies = driver.get_cookies()
+# print(cookies)
